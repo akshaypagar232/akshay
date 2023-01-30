47973e9d9b9a928571b5a413e8102c08a87dce5c
akshaypagar232 <akshaypagar232@gmail.com>
2023-01-30 17:26:08 +0530

    change

---
+++ src/test/java/com/example/demo/ReusedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReusedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
